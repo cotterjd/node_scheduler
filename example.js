040075577ed65b583b468cd9d58ebcc929b5e1f4
cotterjd <jrdn.cttr@gmail.com>
2016-11-29 11:21:26 -0600

    formatting

--- example.js
+++ example.js
@@ -1,8 +1,8 @@
 const
-	scheduler = require('./scheduler'),
+  scheduler = require('./scheduler'),
   func = function () {
-		console.log("I work!");
-	};
+    console.log("I work!");
+  };
 
 //if it's 11:00 AM
 scheduler(func, 11, 0, 3000);

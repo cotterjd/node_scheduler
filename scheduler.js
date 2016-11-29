4c7bb87807bb2a2c07378595294f59d70e44691a
cotterjd <jrdn.cttr@gmail.com>
2016-11-29 11:23:35 -0600

    formatting

--- scheduler.js
+++ scheduler.js
@@ -1,28 +1,28 @@
 const
-	moment = require('moment')
+  moment = require('moment')
 , log = console.log
 , checkEvery_minutes = 10
-,	timeoutFunc = function (func, hour, minute, interval) { 
-		const
-			now = moment().format("HH:mm"),
-			fiveTill = moment().hour(hour).minute(minute - checkEvery_minutes/2).format("HH:mm"),
-			fiveAfter = moment().hour(hour).minute(minute + checkEvery_minutes/2).format("HH:mm"),
-			isRightTime = now > fiveTill && now < fiveAfter 
-			isRightDay = moment().day() != 0 && moment().day() != 6,
-		  triggered = isRightDay;
+, timeoutFunc = function (func, hour, minute, interval) { 
+    const
+      now = moment().format("HH:mm"),
+      fiveTill = moment().hour(hour).minute(minute - checkEvery_minutes/2).format("HH:mm"),
+      fiveAfter = moment().hour(hour).minute(minute + checkEvery_minutes/2).format("HH:mm"),
+      isRightTime = now > fiveTill && now < fiveAfter,
+      isRightDay = moment().day() != 0 && moment().day() != 6,
+      triggered = isRightDay;
 		
-		if(triggered) {
-			func();
-		}
-		log (moment().format("MM/DD/YYYY HH:mm:ss") + ": running...waiting for next trigger");
-		checkTime(func, hour, minute, interval);
-	}
+    if(triggered) {
+      func();
+    }
+    log (moment().format("MM/DD/YYYY HH:mm:ss") + ": running...waiting for next trigger");
+    checkTime(func, hour, minute, interval);
+  }
 , checkTime = function checkTime (func, hour, minute, interval){
-		hour = hour || 8;
-		minute = minute || 30;
-		interval = interval || 600000;
-		setTimeout(timeoutFunc.bind(this, func, hour, minute, interval), interval);
-	}
+    hour = hour || 8;
+    minute = minute || 30;
+    interval = interval || 600000;
+    setTimeout(timeoutFunc.bind(this, func, hour, minute, interval), interval);
+  }
 
 , nil = null;
 

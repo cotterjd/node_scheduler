9db84937c82ccb1cfeca6e99d9520cd115a619dd
cotterjd <jrdn.cttr@gmail.com>
2016-11-30 09:39:30 -0600

    Update example.js

--- example.js
+++ example.js
@@ -4,5 +4,6 @@ const
     console.log("I work!");
   };
 
-//if it's 11:00 AM (pass in 22 if it's 11pm)
+//if it's 11:00 AM (pass in 22 if it's 11pm). 
+//3000 is just for testing. scheduler(func, 11, 0) is all you would want for a daily task running at 11ish everyday
 scheduler(func, 11, 0, 3000);

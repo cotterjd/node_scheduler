98071d5919e8c776827e06bbd99071e744db66fc
Jordan Cotter <jrdn.cttr@gmail.com>
2016-12-28 16:52:40 -0600

    Update example.js

--- example.js
+++ example.js
@@ -5,5 +5,6 @@ const
   };
 
 //if it's 11:00 AM (pass in 22 if it's 11pm). 
-//3000 is just for testing. scheduler(func, 11, 0) is all you would want for a daily task running at 11ish everyday
+//3000 is just for testing. It means the scheduler will check the time every 3 seconds to see if it's time to run the function
+//600000 (10 minutes) is the default if left blank.
 scheduler(func, 11, 0, 3000);

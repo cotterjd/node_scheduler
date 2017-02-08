600b4bad68ba806aef322adbbd4c22e6e71de53f
Jordan Cotter <jrdn.cttr@gmail.com>
2017-02-08 17:03:09 -0600

    scheduler can handle multiple functions now

--- example.js
+++ example.js
@@ -2,9 +2,35 @@ const
   scheduler = require('./scheduler'),
   func = function () {
     console.log("I work!");
-  };
+  },
+	func1 = function () {
+		console.log("I also work!");
+	}, 
+	func2 = function () {
+		console.log("I work too!");
+	},
+	obj0 = {
+		func: func,
+		hour: 16,
+		minute: 59,
+		weekends: true
+	}, 
+	obj1 = {
+		func: func1,
+		hour: 16,
+		minute: 58,
+		weekends: true
+	}, 
+	obj2 = {
+		func: func2,
+		hour: 16,
+		minute: 59,
+		weekends: true
+	}
+	funcs = [obj0, obj1, obj2]
+	;
 
 //if it's 11:00 AM (pass in 22 if it's 11pm). 
 //3000 is just for testing. It means the scheduler will check the time every 3 seconds to see if it's time to run the function
 //600000 (10 minutes) is the default if left blank.
-scheduler(func, 15, 28, 3000);
+scheduler(funcs, 3000);
--- scheduler.js
+++ scheduler.js
@@ -1,5 +1,7 @@
 var
   moment = require('moment')
+, R = require('ramda')
+, async = require('async')
 , log = console.log
 , firstRun = true 
 , makeConversions = function (milliseconds) {
@@ -16,49 +18,68 @@ var
 				h: h
 			}
 	}
-, timeoutFunc = function (func, hour, minute, interval, lowerBound, upperBound, weekends) { 
-    const
-      now = moment().format("HH:mm:ss.SSS"),
-      isRightTime = now >= lowerBound && now <= upperBound,
-      isRightDay = weekends ? true : moment().day() != 0 && moment().day() != 6,
-      triggered = isRightDay && isRightTime;
-		
-    if(triggered) {
-      log("triggered at " + moment().format("MM/DD/YYYY HH:mm:ss.SSS"));
-      func();
-    	checkTime(func, hour, minute, interval);
-    } else {
-      log (moment().format("MM/DD/YYYY HH:mm:ss.SSS") + ": running...will trigger between " + lowerBound + " and " + upperBound);
-    	checkTime(func, hour, minute, interval, weekends);
-    }
+, getAsyncFunctions = function (obj) {
+		return function (cb) {
+    	const
+    	  now = moment().format("MM/DD/YYYY HH:mm:ss.SSS"),
+    	  isRightTime = now >= obj.lowerBound && now <= obj.upperBound,
+    	  isRightDay = obj.weekends ? true : moment().day() != 0 && moment().day() != 6,
+    	  triggered = isRightDay && isRightTime;
+			
+    	if(triggered) {
+    	  log(obj.func.name + " triggered at " + now);
+    	  obj.func();
+    	} else {
+    	  log (now + ": running...will trigger " + obj.func.name + " between " + obj.lowerBound + " and " + obj.upperBound);
+    	}
+
+			return cb()
+		}
+	}
+, checkTime = function (funcs, interval) { 
+		const asyncFuncs = R.map(getAsyncFunctions, funcs)	
+		;
+
+		async.series(asyncFuncs, function (err, results) {
+			if(err) log(err);
+			setTimeout(checkTime.bind(this, funcs, interval), interval);		
+		});
   }
-, checkTime = function checkTime (func, hour, minute, interval, weekends){
-    hour = hour != null ? hour : 8;
-    minute = minute != null ? minute : 30;
-    interval = interval != null ? interval : 600000;
-		
+, getDefaults = function (obj) {
+		obj.hour = obj.hour != null ? obj.hour : 8;
+		obj.minute = obj.minute != null ? obj.minute : 30;
+		return obj;
+ 	}
+, getBounds = R.curry(function (interval, obj) {
 		const 
 			  timeObj = makeConversions(interval/2)
 			, lowerBound = moment()
-					.hour(hour - timeObj.h)
-					.minute(minute - timeObj.m)
+					.hour(obj.hour - timeObj.h)
+					.minute(obj.minute - timeObj.m)
 					.seconds(0 - timeObj.s)
 					.milliseconds(0 - timeObj.ms)
-					.format("HH:mm:ss.SSS")
+					.format("MM/DD/YYYY HH:mm:ss.SSS")
 			, upperBound = moment()
-					.hour(hour + timeObj.h)
-					.minute(minute + timeObj.m)
+					.hour(obj.hour + timeObj.h)
+					.minute(obj.minute + timeObj.m)
 					.seconds(timeObj.s)
 					.milliseconds(timeObj.ms)
-					.format("HH:mm:ss.SSS")
-    if (firstRun) {
-			log (moment().format("MM/DD/YYYY HH:mm:ss.SSS") 
-				+ ": running...will trigger between " 
-				+ lowerBound + " and " + upperBound);
-			firstRun = false;
-		}
-    setTimeout(timeoutFunc.bind(this, func, hour, minute, interval, lowerBound, upperBound, weekends), interval);
+					.format("MM/DD/YYYY HH:mm:ss.SSS")
+			;
+
+		log (moment().format("MM/DD/YYYY HH:mm:ss.SSS") 
+			+ ": running...will trigger " + obj.func.name + " between " 
+			+ lowerBound + " and " + upperBound);
+		obj.lowerBound = lowerBound;
+		obj.upperBound = upperBound;
+		return obj;
+ 	})
+, main = function (funcs, interval){
+    interval = interval != null ? interval : 600000;
+		
+		const funcObjects = R.pipe(R.map(getDefaults), R.map(getBounds(interval)))(funcs)
+    setTimeout(checkTime.bind(this, funcObjects, interval), interval);
   }
 , nil = null;
 
-module.exports = checkTime 
+module.exports = main;

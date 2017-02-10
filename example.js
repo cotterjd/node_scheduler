b1dda79594da3b572ca0d615985e3e658aa168f1
Jordan Cotter <jrdn.cttr@gmail.com>
2017-02-10 08:32:51 -0600

    updated comments on example and package.json

--- example.js
+++ example.js
@@ -1,3 +1,4 @@
+//example is for running 3 functions at 4:30 pm
 const
   scheduler = require('./scheduler'),
   func = function () {
@@ -12,25 +13,24 @@ const
 	obj0 = {
 		func: func,
 		hour: 16,
-		minute: 59,
+		minute: 30,
 		weekends: true
 	}, 
 	obj1 = {
 		func: func1,
 		hour: 16,
-		minute: 58,
+		minute: 30,
 		weekends: true
 	}, 
 	obj2 = {
 		func: func2,
 		hour: 16,
-		minute: 59,
+		minute: 30,
 		weekends: true
 	}
 	funcs = [obj0, obj1, obj2]
 	;
 
-//if it's 11:00 AM (pass in 22 if it's 11pm). 
 //3000 is just for testing. It means the scheduler will check the time every 3 seconds to see if it's time to run the function
 //600000 (10 minutes) is the default if left blank.
 scheduler(funcs, 3000);

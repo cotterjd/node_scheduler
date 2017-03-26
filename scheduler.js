42e11c5a3eaca0b1f59aec2b792f98995cf60cec
Jordan Cotter <jrdn.cttr@gmail.com>
2017-03-26 12:35:54 -0500

    git screwing up my formatting..

--- scheduler.js
+++ scheduler.js
@@ -6,37 +6,37 @@ var
 , logI = x => {log(x); return x;} 
 , firstRun = true 
 , makeConversions = function (milliseconds) {
-		const
-			ms= Math.floor((milliseconds%1000)),
-			s = Math.floor((milliseconds/1000)%60),
-			m = Math.floor((milliseconds/(1000*60))%24),
-			h = Math.floor((milliseconds/(1000*60*60))%24);
+	const
+		ms= Math.floor((milliseconds%1000)),
+		s = Math.floor((milliseconds/1000)%60),
+		m = Math.floor((milliseconds/(1000*60))%24),
+		h = Math.floor((milliseconds/(1000*60*60))%24);
 
-			return {
-				ms: ms,
-				s: s,
-				m: m,
-				h: h
-			}
-	}
+		return {
+			ms: ms,
+			s: s,
+			m: m,
+			h: h
+		}
+  }
 , setColor = x => "\x1b["+x+"m"
 , resetColor = () => "\x1b[0m"
 , longFormat = "MM/DD/YYYY HH:mm:ss:SSS"
 , getAsyncFunctions = function (obj) {
-		return function (cb) {
+     return function (cb) {
     	const
     	  now = moment().format(longFormat),
     	  isRightTime = now >= obj.lowerBound && now <= obj.upperBound,
     	  isRightDay = obj.weekends ? true : moment().day() != 0 && moment().day() != 6,
     	  triggered = isRightDay && isRightTime,
-				setcolor = setColor(obj.color),
-				functionName = setcolor + obj.func.name + resetColor(),
-				triggerTime = obj.hour + ": " + obj.minute,
-				lowerBoundTime = moment(obj.lowerBound, longFormat).format("hh:mm a"),
-				upperBoundTime = moment(obj.upperBound, longFormat).format("hh:mm a"),
-				day = moment(obj.upperBound, longFormat).format("ddd MMM Do"),
-				range = setcolor + lowerBoundTime + resetColor() + " and " + setcolor + upperBoundTime + resetColor() + " on " + day 
-				;
+	  setcolor = setColor(obj.color),
+	  functionName = setcolor + obj.func.name + resetColor(),
+	  triggerTime = obj.hour + ": " + obj.minute,
+	  lowerBoundTime = moment(obj.lowerBound, longFormat).format("hh:mm a"),
+	  upperBoundTime = moment(obj.upperBound, longFormat).format("hh:mm a"),
+	  day = moment(obj.upperBound, longFormat).format("ddd MMM Do"),
+	  range = setcolor + lowerBoundTime + resetColor() + " and " + setcolor + upperBoundTime + resetColor() + " on " + day 
+	;
 			
     	if(triggered) {
     	  log(obj.func.name + " triggered at " + now);
@@ -45,21 +45,21 @@ var
     	  log (now + ": running...will trigger " + functionName + " between " + range);
     	}
 
-			return cb()
-		}
-	}
+	return cb()
+    }
+  }
 , randomColorNum = function () {
-		const 
-			colorNums = [31, 32, 33, 34, 35, 36]
+	const 
+		colorNums = [31, 32, 33, 34, 35, 36]
 		, index = Math.round(Math.random() * (colorNums.length-1));
 
 		return colorNums[index];
-	}
+  }
 , checkTime = function (funcObjects, interval) { 
-		const 
+	const 
 		color = randomColorNum(),
 		asyncFuncs = R.pipe(
-				R.map(x => {x.color=color; return x})
+                  	  R.map(x => {x.color=color; return x})
 			, R.map(getAsyncFunctions)
 			)(R.map(getBounds(interval), funcObjects))	
 		;
@@ -108,7 +108,7 @@ var
  	})
 , logTimes = R.curry(function (color, obj) {
 		const 
-			format = "ddd MMM Do"
+		  format = "ddd MMM Do"
 		, date = obj.date ? (triggerDateObject(obj) < moment().format(longFormat) ? moment().month(moment().month() + 1).date(obj.date).format(format) : moment().date(obj.date).format(format)) : moment().format(format);
 		log("running. " + setColor(color) + obj.func.name + resetColor() + " will be triggered around " + setColor(color) + obj.hour + ":" + obj.minute + resetColor() + " on " + date);
 	})

b4d18f0241d245a1594c29ce2a9d543ad466624b
Jordan Cotter <jrdn.cttr@gmail.com>
2020-09-10 10:54:40 -0500

    fix formatting

--- example.js
+++ example.js
@@ -4,34 +4,34 @@ const
   func = function () {
     console.log("I work!");
   },
-	func1 = function () {
-		console.log("I also work!");
-	}, 
-	func2 = function () {
-		console.log("I work too!");
-	},
-	obj0 = {
-		func: func,
-		hour: 16,
-		minute: 30,
-		weekends: true, //will run on weekends also
-		date: 14  //will only run on the 14th of each month
-	}, 
-	obj1 = {
-		func: func1,
-		hour: 16,
-		minute: 30,
-		date: 15  //will not run on weekends even if the 15th falls on a weekend since weekends is not set to true
-	}, 
-	//will run at 4:30 every day including weekends
-	obj2 = {
-		func: func2,
-		hour: 16,
-		minute: 30,
-		weekends: true,
-	}
-	funcs = [obj0, obj1, obj2]
-	;
+  func1 = function () {
+    console.log("I also work!");
+  }, 
+  func2 = function () {
+    console.log("I work too!");
+  },
+  obj0 = {
+    func: func,
+    hour: 16,
+    minute: 30,
+    weekends: true, //will run on weekends also
+    date: 14  //will only run on the 14th of each month
+  }, 
+  obj1 = {
+    func: func1,
+    hour: 16,
+    minute: 30,
+    date: 15  //will not run on weekends even if the 15th falls on a weekend since weekends is not set to true
+  }, 
+  //will run at 4:30 every day including weekends
+  obj2 = {
+    func: func2,
+    hour: 16,
+    minute: 30,
+    weekends: true,
+  }
+  funcs = [obj0, obj1, obj2]
+  ;
 
 //3000 is just for testing. It means the scheduler will check the time every 3 seconds to see if it's time to run the function
 //600000 (10 minutes) is the default if left blank.
--- scheduler.js
+++ scheduler.js
@@ -6,117 +6,117 @@ var
 , logI = x => {log(x); return x;} 
 , firstRun = true 
 , makeConversions = function (milliseconds) {
-	const
-		ms= Math.floor((milliseconds%1000)),
-		s = Math.floor((milliseconds/1000)%60),
-		m = Math.floor((milliseconds/(1000*60))%24),
-		h = Math.floor((milliseconds/(1000*60*60))%24);
+  const
+    ms= Math.floor((milliseconds%1000)),
+    s = Math.floor((milliseconds/1000)%60),
+    m = Math.floor((milliseconds/(1000*60))%24),
+    h = Math.floor((milliseconds/(1000*60*60))%24);
 
-		return {
-			ms: ms,
-			s: s,
-			m: m,
-			h: h
-		}
+    return {
+      ms: ms,
+      s: s,
+      m: m,
+      h: h
+    }
   }
 , setColor = x => "\x1b["+x+"m"
 , resetColor = () => "\x1b[0m"
 , longFormat = "MM/DD/YYYY HH:mm:ss:SSS"
 , getAsyncFunctions = function (obj) {
      return function (cb) {
-    	const
-    	  now = moment().format(longFormat),
-    	  isRightTime = now >= obj.lowerBound && now <= obj.upperBound,
-    	  isRightDay = obj.weekends ? true : moment().day() != 0 && moment().day() != 6,
-    	  triggered = isRightDay && isRightTime,
-	  setcolor = setColor(obj.color),
-	  functionName = setcolor + obj.func.name + resetColor(),
-	  triggerTime = obj.hour + ": " + obj.minute,
-	  lowerBoundTime = moment(obj.lowerBound, longFormat).format("hh:mm a"),
-	  upperBoundTime = moment(obj.upperBound, longFormat).format("hh:mm a"),
-	  day = moment(obj.upperBound, longFormat).format("ddd MMM Do"),
-	  range = setcolor + lowerBoundTime + resetColor() + " and " + setcolor + upperBoundTime + resetColor() + " on " + day 
-	;
-			
-    	if(triggered) {
-    	  log(obj.func.name + " triggered at " + now);
-    	  obj.func();
-    	} else {
-    	  log (now + ": running...will trigger " + functionName + " between " + range);
-    	}
+      const
+        now = moment().format(longFormat),
+        isRightTime = now >= obj.lowerBound && now <= obj.upperBound,
+        isRightDay = obj.weekends ? true : moment().day() != 0 && moment().day() != 6,
+        triggered = isRightDay && isRightTime,
+    setcolor = setColor(obj.color),
+    functionName = setcolor + obj.func.name + resetColor(),
+    triggerTime = obj.hour + ": " + obj.minute,
+    lowerBoundTime = moment(obj.lowerBound, longFormat).format("hh:mm a"),
+    upperBoundTime = moment(obj.upperBound, longFormat).format("hh:mm a"),
+    day = moment(obj.upperBound, longFormat).format("ddd MMM Do"),
+    range = setcolor + lowerBoundTime + resetColor() + " and " + setcolor + upperBoundTime + resetColor() + " on " + day 
+  ;
+      
+      if(triggered) {
+        log(obj.func.name + " triggered at " + now);
+        obj.func();
+      } else {
+        log (now + ": running...will trigger " + functionName + " between " + range);
+      }
 
-	return cb()
+  return cb()
     }
   }
 , randomColorNum = function () {
-	const 
-		colorNums = [31, 32, 33, 34, 35, 36]
-		, index = Math.round(Math.random() * (colorNums.length-1));
+  const 
+    colorNums = [31, 32, 33, 34, 35, 36]
+    , index = Math.round(Math.random() * (colorNums.length-1));
 
-		return colorNums[index];
+    return colorNums[index];
   }
 , checkTime = function (funcObjects, interval) { 
-	const 
-		color = randomColorNum(),
-		asyncFuncs = R.pipe(
-                  	  R.map(x => {x.color=color; return x})
-			, R.map(getAsyncFunctions)
-			)(R.map(getBounds(interval), funcObjects))	
-		;
+  const 
+    color = randomColorNum(),
+    asyncFuncs = R.pipe(
+                      R.map(x => {x.color=color; return x})
+      , R.map(getAsyncFunctions)
+      )(R.map(getBounds(interval), funcObjects))  
+    ;
 
-		async.series(asyncFuncs, function (err, results) {
-			if(err) log(err);
-			setTimeout(checkTime.bind(this, funcObjects, interval), interval);		
-		});
+    async.series(asyncFuncs, function (err, results) {
+      if(err) log(err);
+      setTimeout(checkTime.bind(this, funcObjects, interval), interval);    
+    });
   }
 , getDefaults = function (obj) {
-		obj.hour = obj.hour != null ? obj.hour : 8;
-		obj.minute = obj.minute != null ? obj.minute : 30;
-		return obj;
- 	}
+    obj.hour = obj.hour != null ? obj.hour : 8;
+    obj.minute = obj.minute != null ? obj.minute : 30;
+    return obj;
+  }
 , triggerDateObject = (obj) => {
-		if (obj.date) return moment().date(obj.date).hour(obj.hour).minute(obj.minute).format(longFormat);
-		else throw "No date property on object"
-	}
+    if (obj.date) return moment().date(obj.date).hour(obj.hour).minute(obj.minute).format(longFormat);
+    else throw "No date property on object"
+  }
 , today = f => moment().format(f)
 , getBounds = R.curry(function (interval, obj) {
-		const 
-			  timeObj = makeConversions(interval/2)
-			, triggerDayHasPassed = obj.date ? triggerDateObject(obj) < today(longFormat) : false
-			, incrementMonth = moment().month(moment().month() + 1).date(obj.date || moment().date()).format(longFormat)
-			, format = "MM/DD/YYYY"
-			, date = obj.date ? 
-					(triggerDayHasPassed ? incrementMonth : moment().date(obj.date).format(longFormat)) : 
-					today(longFormat)
-			, lowerBound = moment(date, longFormat)
-					.hour(obj.hour - timeObj.h)
-					.minute(obj.minute - timeObj.m)
-					.seconds(0 - timeObj.s)
-					.milliseconds(0 - timeObj.ms)
-					.format(longFormat)
-			, upperBound = moment(date, longFormat)
-					.hour(obj.hour + timeObj.h)
-					.minute(obj.minute + timeObj.m)
-					.seconds(timeObj.s)
-					.milliseconds(timeObj.ms)
-					.format(longFormat)
-			;
+    const 
+        timeObj = makeConversions(interval/2)
+      , triggerDayHasPassed = obj.date ? triggerDateObject(obj) < today(longFormat) : false
+      , incrementMonth = moment().month(moment().month() + 1).date(obj.date || moment().date()).format(longFormat)
+      , format = "MM/DD/YYYY"
+      , date = obj.date ? 
+          (triggerDayHasPassed ? incrementMonth : moment().date(obj.date).format(longFormat)) : 
+          today(longFormat)
+      , lowerBound = moment(date, longFormat)
+          .hour(obj.hour - timeObj.h)
+          .minute(obj.minute - timeObj.m)
+          .seconds(0 - timeObj.s)
+          .milliseconds(0 - timeObj.ms)
+          .format(longFormat)
+      , upperBound = moment(date, longFormat)
+          .hour(obj.hour + timeObj.h)
+          .minute(obj.minute + timeObj.m)
+          .seconds(timeObj.s)
+          .milliseconds(timeObj.ms)
+          .format(longFormat)
+      ;
 
-		obj.lowerBound = lowerBound;
-		obj.upperBound = upperBound;
-		return obj;
- 	})
+    obj.lowerBound = lowerBound;
+    obj.upperBound = upperBound;
+    return obj;
+  })
 , logTimes = R.curry(function (color, obj) {
-		const 
-		  format = "ddd MMM Do"
-		, date = obj.date ? (triggerDateObject(obj) < moment().format(longFormat) ? moment().month(moment().month() + 1).date(obj.date).format(format) : moment().date(obj.date).format(format)) : moment().format(format);
-		log("running. " + setColor(color) + obj.func.name + resetColor() + " will be triggered around " + setColor(color) + obj.hour + ":" + obj.minute + resetColor() + " on " + date);
-	})
+    const 
+      format = "ddd MMM Do"
+    , date = obj.date ? (triggerDateObject(obj) < moment().format(longFormat) ? moment().month(moment().month() + 1).date(obj.date).format(format) : moment().date(obj.date).format(format)) : moment().format(format);
+    log("running. " + setColor(color) + obj.func.name + resetColor() + " will be triggered around " + setColor(color) + obj.hour + ":" + obj.minute + resetColor() + " on " + date);
+  })
 , main = function (objs, interval = 600000){
-		
-		const funcObjects = R.map(getDefaults, objs);
+    
+    const funcObjects = R.map(getDefaults, objs);
 
-		funcObjects.forEach(logTimes(randomColorNum()));
+    funcObjects.forEach(logTimes(randomColorNum()));
     setTimeout(checkTime.bind(this, funcObjects, interval), interval);
   }
 , nil = null;
